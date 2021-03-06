81c7a488b9dd302734be86f07fc93106910804d7
Szántó Dániel <sznt.daniel@gmail.com>
2021-03-06 15:13:42 +0100

    updated HelloMe

--- java-test-week02/src/HelloMe.java
+++ java-test-week02/src/HelloMe.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class HelloMe {
+public class HelloMe {
+    public static void main(String[] args) {
+        // Modify this program to greet you instead of the World!
+        System.out.println("Hello, Dani!");
+    }
 }
+
