985c5e3738640e4c037c38531358371d487e00cd
Szántó Dániel <sznt.daniel@gmail.com>
2021-03-06 15:26:57 +0100

    Added HelloClass.java

---
+++ java-test-week02/src/HelloClass.java
@@ -0,0 +1,20 @@
+
+
+public class HelloClass {
+        public static void main(String[] args) {
+            // Greet 3 of your classmates with this program in three separate lines
+            // like:
+            //
+            // Hello, Esther!
+            // Hello, Mary!
+            // Hello, Joe!
+
+            System.out.println("Hello, András!");
+            System.out.println("Hello, Barbi!");
+            System.out.println("Hello, Zoli!");
+
+        }
+    }
+
+
+
