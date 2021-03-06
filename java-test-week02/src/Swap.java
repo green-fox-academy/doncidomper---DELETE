1c0af04d73f0100b1fd493c9c2d089fafe51affc
Szántó Dániel <sznt.daniel@gmail.com>
2021-03-06 16:37:56 +0100

    Added Swap.java

---
+++ java-test-week02/src/Swap.java
@@ -0,0 +1,19 @@
+public class Swap {
+
+    public static void main(String[] args) {
+        // Swap the values of the variables
+
+        int a , b , temp;
+
+         a = 123;
+         b = 526;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
