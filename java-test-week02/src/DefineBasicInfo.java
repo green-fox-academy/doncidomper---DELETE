7b937e2125a52a40b86bbab284d6e3699880b438
Szántó Dániel <sznt.daniel@gmail.com>
2021-03-06 17:00:38 +0100

    Added DefineBasicInfo.java

---
+++ java-test-week02/src/DefineBasicInfo.java
@@ -0,0 +1,25 @@
+public class DefineBasicInfo {
+
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+
+        int age = 30;
+        double height = 1.72;
+        boolean status = true;
+
+
+        System.out.println("Name: Szántó Dániel");
+        System.out.println("Age:" + age );
+        System.out.println("Height:" + height  );
+        System.out.println(status);
+
+
+
+
+    }
+
+}
