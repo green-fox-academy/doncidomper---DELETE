9bf25834e5cb58cefc97fc30c1e634c7aede6fdd
Szántó Dániel <sznt.daniel@gmail.com>
2021-03-06 19:52:43 +0100

    Added final VariableMutation.java

--- java-test-week02/src/VariableMutation.java
+++ java-test-week02/src/VariableMutation.java
@@ -29,29 +29,43 @@ public class VariableMutation {
         int f1 = 123;
         int f2 = 345;
 
-        if (f1>f2)
-            boolean bigger = true;
-        System.out.println(true);
+
+            boolean biggerF = f2<f1;
+        System.out.println("f2 < f1 " + biggerF );
 
         // tell if f1 is bigger than f2 (print as a boolean)
 
         int g1 = 350;
         int g2 = 200;
         // tell if the double of g2 is bigger than g1 (print as a boolean)
-        if (g1<g2)
-            boolean bigger=false;
-        System.out.println(false);
+            boolean biggerG= g1<(g2*2);
+        System.out.println("g2 * 2 < g1 " + biggerG);
 
 
         int h = 135798745;
         // tell if it has 11 as a divisor (print as a boolean)
-if ()
+
+        boolean hisDivisibleBy11 = h % 11 == 0;
+
+        System.out.println("h is divisible by 11: " + hisDivisibleBy11);
+
         int i1 = 10;
         int i2 = 3;
         // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
 
+        boolean higher = i1 > (i2^2);
+        System.out.println("i1 > i2^2: " + higher );
+        boolean smaller = i1 < (i2^3);
+        System.out.println("i1 < i2^3  " + smaller );
+
         int j = 1521;
         // tell if j is dividable by 3 or 5 (print as a boolean)
+
+        boolean jisDivisibleBy35 = (j % 3 == 0 || j % 5 == 0);
+        System.out.println("j is divisible by 3 or 5: " + jisDivisibleBy35 );
+
+
+
     }
 
 }
